458c373949bd2dbe512730522c6d70bcefca8647
zdrigo171 <69566608+zdrigo171@users.noreply.github.com>
2024-02-17 12:13:48 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Adicione o link da WenHook nas aspas simples
+var wbhk = 'https://discord.com/api/webhooks/1208439604794105856/pNQboVxkQsD3eo5me_T9OD8NuS4D6GQ4dK4anYf4hp2A2fBVaJ2RNk02DUoDQgo5GOLZ'
